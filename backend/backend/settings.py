850d7440cff4f99d1dd02dcbbf99bff3c70986bd
shrikaran ks <shrikaran.ks@think41.com>
2024-08-06 10:30:55 +0530

    made all necessary changes changed the db to postgres again ready for deplyment

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-
+# //necesary
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
